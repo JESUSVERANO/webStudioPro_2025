7856ddabbb168938d44b2a214582a12aa3c69ea7
Jesús verano <95895331+JESUSVERANO@users.noreply.github.com>
2025-12-07 21:57:57 -0500

    Avance de reportes

---
+++ webStudioPro/WebStudioPro.Presentacion/FrmMain.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WebStudioPro.Presentacion
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cotizadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proyectosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.leadsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.leadsToolStripMenuItem,
+            this.cotizadorToolStripMenuItem,
+            this.proyectosToolStripMenuItem,
+            this.clientesToolStripMenuItem,
+            this.blogToolStripMenuItem,
+            this.reportesToolStripMenuItem});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.menuPrincipal.TabIndex = 1;
+            this.menuPrincipal.Text = "menuStrip1";
+            this.menuPrincipal.FontChanged += new System.EventHandler(this.menuPrincipal_FontChanged);
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            this.clientesToolStripMenuItem.Click += new System.EventHandler(this.clientesToolStripMenuItem_Click);
+            // 
+            // cotizadorToolStripMenuItem
+            // 
+            this.cotizadorToolStripMenuItem.Name = "cotizadorToolStripMenuItem";
+            this.cotizadorToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.cotizadorToolStripMenuItem.Text = "Cotizador";
+            this.cotizadorToolStripMenuItem.Click += new System.EventHandler(this.cotizadorToolStripMenuItem_Click);
+            // 
+            // proyectosToolStripMenuItem
+            // 
+            this.proyectosToolStripMenuItem.Name = "proyectosToolStripMenuItem";
+            this.proyectosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.proyectosToolStripMenuItem.Text = "Proyectos";
+            this.proyectosToolStripMenuItem.Click += new System.EventHandler(this.proyectosToolStripMenuItem_Click);
+            // 
+            // blogToolStripMenuItem
+            // 
+            this.blogToolStripMenuItem.Name = "blogToolStripMenuItem";
+            this.blogToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.blogToolStripMenuItem.Text = "Blog";
+            this.blogToolStripMenuItem.Click += new System.EventHandler(this.blogToolStripMenuItem_Click);
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientesToolStripMenuItem1});
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            this.reportesToolStripMenuItem.Click += new System.EventHandler(this.reportesToolStripMenuItem_Click);
+            // 
+            // clientesToolStripMenuItem1
+            // 
+            this.clientesToolStripMenuItem1.Name = "clientesToolStripMenuItem1";
+            this.clientesToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+            this.clientesToolStripMenuItem1.Text = "Clientes";
+            // 
+            // leadsToolStripMenuItem
+            // 
+            this.leadsToolStripMenuItem.Name = "leadsToolStripMenuItem";
+            this.leadsToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.leadsToolStripMenuItem.Text = "Leads";
+            this.leadsToolStripMenuItem.Click += new System.EventHandler(this.leadsToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuPrincipal);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuPrincipal;
+            this.Name = "FrmMain";
+            this.Text = "WebStudio PRO - Panel Principal";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proyectosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cotizadorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem leadsToolStripMenuItem;
+    }
+}
---
+++ webStudioPro/WebStudioPro.Presentacion/FrmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+using WebStudioPro.Presentacion.Entities;   // 👈 IMPORTANTE
+
+namespace WebStudioPro.Presentacion
+{
+    public partial class FrmMain : Form
+    {
+        private readonly Usuario _usuarioActual;
+
+        // Constructor usado desde el login
+        public FrmMain(Usuario usuario) : this()
+        {
+            _usuarioActual = usuario;
+
+            if (_usuarioActual != null && !string.IsNullOrWhiteSpace(_usuarioActual.Nombre))
+            {
+                this.Text = $"WebStudio PRO - Bienvenido, {_usuarioActual.Nombre}";
+            }
+        }
+
+        // Constructor sin parámetros (necesario para el diseñador)
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        // 🔥 ESTE MÉTODO ES EL QUE FALTABA
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            // Opcional: Maximizar al abrir
+            this.WindowState = FormWindowState.Maximized;
+
+            // Opcional: Mensaje en la barra de título si no vino usuario
+            if (_usuarioActual == null)
+            {
+                this.Text = "WebStudio PRO - Panel Principal";
+            }
+        }
+
+        private void OpenChild(Form child)
+        {
+            child.MdiParent = this;
+            child.WindowState = FormWindowState.Maximized;
+            child.Show();
+        }
+
+        private void clientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChild(new FrmClientes());
+        }
+
+        private void cotizadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChild(new FrmCotizador());
+        }
+
+        private void proyectosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChild(new FrmProyectos());
+        }
+
+        private void portafolioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChild(new FrmPortafolio());
+        }
+
+        private void blogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChild(new FrmBlog());
+        }
+
+        private void reportesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenChild(new FrmReporteClientes());
+        }
+
+        private void menuPrincipal_FontChanged(object sender, EventArgs e)
+        {
+            // Evento autogenerado, no se usa
+        }
+
+        private void leadsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new FrmLeads();
+            frm.MdiParent = this;
+            frm.Show();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
